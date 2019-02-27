b5f605ac3a9a1789ea9f1c5c24a6713ea6589529
Bushra Al-dosary <baldosary@kku.edu.sa>
2019-02-27 12:43:30 +0300

    Add test cases for GET /todos/:id

--- Server/Test/server.test.js
+++ Server/Test/server.test.js
@@ -2,9 +2,10 @@ const request = require('supertest');
 const expect = require('expect');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
-
+const {ObjectID} = require('mongodb');
 
 const todo = [{
+    _id: new ObjectID(),
     text: 'first todo'
 },
 {
@@ -18,6 +19,7 @@ beforeEach((done) =>
     }
    ).then(()=> done());
 });
+//Testing POST route:
 describe('POST /todos', () =>
 {
     it('Should create a new todo', (done) => 
@@ -62,7 +64,7 @@ describe('POST /todos', () =>
         })
     })
 });
-
+//Testing GET route:
 describe('GET /todos', () =>
 {
     it('It should get all todos', (done) =>
@@ -77,3 +79,36 @@ describe('GET /todos', () =>
         .end(done);
     })
 });
+//Testing GET route with specific ID:
+
+describe('GET /todo/:id', () => 
+{
+    it('It should return todos for the specific IDs', (done) => 
+    {
+        request(app)
+        .get(`/todos/${todo[0]._id.toHexString()}`)
+        .expect(200)
+        .expect(res =>
+         {
+             expect(res.body.todos.text).toBe(todo[0].text);
+         })
+         .end(done);
+    });
+    it('It should return 404 for non-object id', (done) => 
+    {
+        const id = '5c756a6bca2692a49dbb1685111';
+
+        request(app)
+        .get(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+    it('It should returns 400 if todo not found', (done) => 
+    {
+        const id = new ObjectID('5c766a6bca2692a49dbb1685') 
+        request(app)
+        .get(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+})
